b0eebee00ce0d0822857c6d7705bc8f8a80f9b93
Leonardo Ojeda <perros_rebeldes@hotmail.com>
2026-08-18 00:37:23 -0600

    feat: allow continuous jumping while holding jump

--- player/player.gd
+++ player/player.gd
@@ -116,7 +116,7 @@ func _physics_process(delta: float) -> void:
 
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
 	var input_direction := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
